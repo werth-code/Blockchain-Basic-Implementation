f28a894446f9af8c2910114316be554adf7cd4fb
Matt Werth <matthewwerth@gmail.com>
2021-01-27 14:55:59 -0500

    Added Basic Mining/Proof Of Work

--- firstBlockchain.js
+++ firstBlockchain.js
@@ -7,20 +7,28 @@ class Block {
     this.data = data
     this.prevHash = prevHash
     this.hash = this.calculateHash()
+    this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.prevHash + this.timestamp + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.prevHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
     }
 
-    getIndex() {
-        return this.index
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++
+            this.hash = this.calculateHash();
+        }
+        console.log("Block Mined: " + this.hash)
     }
 }
+
+
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()]
+        this.difficulty = 4;
     }
 
     createGenesisBlock() {
@@ -33,7 +41,7 @@ class Blockchain {
 
     addBlock(newBlock){ //add a new block in our chain
         newBlock.prevHash = this.getLastBlock().hash // set our previous has to the hash value of our last block (think linked list)
-        newBlock.hash = newBlock.calculateHash()
+        newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock)
     }
 
@@ -50,15 +58,20 @@ class Blockchain {
 }
 
 let werthCoin = new Blockchain()
+
+console.log("Mining...")
 werthCoin.addBlock(new Block(1, "2020-01-01", [40]))
+console.log("Mining...")
 werthCoin.addBlock(new Block(2, "2020-01-01", [50]))
 
 console.log(werthCoin)
-console.log(werthCoin.validateChain())
 
-werthCoin.chain[1].data = [5000]
-werthCoin.chain[1].hash = werthCoin.chain[1].calculateHash() // Cannot change value - Even if recalculating hash
 
-console.log(werthCoin.validateChain())
 
-console.log(werthCoin)
+// console.log(werthCoin)
+// console.log(werthCoin.validateChain())
+
+// werthCoin.chain[1].data = [5000]                             // Tests our chain validation
+// werthCoin.chain[1].hash = werthCoin.chain[1].calculateHash() // Cannot change value - Even if recalculating hash
+
+// console.log(werthCoin.validateChain()) 
